fa5c33d50927473cdd3ab713917503116b784d57
Tim Bedford <tmbdfd@gmail.com>
2020-12-01 10:15:49 -0500

    Change config keys format to depend on dev/prod environment and add new npm command for deployment

--- app.js
+++ app.js
@@ -1,8 +1,16 @@
 const mongoose = require('mongoose');
+const path = require('path');
 
 const express = require("express");
 const app = express();
 const db = require('./config/keys').mongoURI;
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('frontend/build'));
+  app.get('/', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
+  })
+}
 
 mongoose
   .connect(db, { useNewUrlParser: true, useUnifiedTopology: true })
