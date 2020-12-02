125cf61bb1da7cf7f9efec44d77e4dfe8c8d3f29
jared-greenberg <jared.p.greenberg@gmail.com>
2020-12-02 12:04:35 -0500

    Create navbar container

---
+++ frontend/src/components/navbar/navbar_container.js
@@ -0,0 +1,14 @@
+import { connect } from 'react-redux';
+import { openModal } from '../../actions/modal_actions';
+import { logout } from '../../actions/session_actions';
+
+const mStP = state => ({
+  currentUser: state.session
+})
+
+const mDtP = dispatch => ({
+  logout: () => dispatch(logout()),
+  openModal: modalType => dispatch(openModal(modalType))
+})
+
+export default connect(mStP, mDtP)(NavBar);
