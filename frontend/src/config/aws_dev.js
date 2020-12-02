24b6a9c57aa7b19b715745743c021f938428add2
jared-greenberg <jared.p.greenberg@gmail.com>
2020-12-01 22:58:23 -0500

    git ignore aws creds

---
+++ frontend/src/config/aws_dev.js
@@ -0,0 +1,5 @@
+module.exports = {
+  BUCKET_NAME: 'menu-tube-dev',
+  ID: 'AKIAXCYZYHEIVF6AMWNL',
+  SECRET: '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'
+}
---
+++ frontend/src/config/aws_prod.js
@@ -0,0 +1,5 @@
+module.exports = {
+  BUCKET_NAME: 'menu-tube-prod',
+  ID: 'AKIAXCYZYHEIVF6AMWNL',
+  SECRET: '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'
+}
