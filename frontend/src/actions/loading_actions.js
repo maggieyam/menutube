ac68f1fa30b8e62f850f22ba06f233502bcba4c5
jared-greenberg <jared.p.greenberg@gmail.com>
2020-12-02 11:30:28 -0500

    add loading actions

---
+++ frontend/src/actions/loading_actions.js
@@ -0,0 +1,10 @@
+export const LOADING_ON = 'LOADING_ON';
+export const LOADING_OFF = 'LOADING_OFF';
+
+export const loadingOn = () => ({
+  type: LOADING_ON
+})
+
+export const loadingOff = () => ({
+  type: LOADING_OFF
+})
