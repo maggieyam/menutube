4fbcd690be9cf57ea27eb6b573025ab372ebe379
jared-greenberg <jared.p.greenberg@gmail.com>
2020-12-01 22:23:14 -0500

    Add receive posts action and reducer case

--- frontend/src/actions/post_actions.js
+++ frontend/src/actions/post_actions.js
@@ -1,12 +1,19 @@
 import * as APIUtil from './post_actions';
 
 export const RECEIVE_POST = 'RECEIVE_POST';
+export const RECEIVE_POSTS = 'RECEIVE_POSTS';
 export const RECEIVE_POST_ERRORS = 'RECEIVE_POST_ERRORS';
 export const CLEAR_POST_ERRORS = 'CLEAR_POST_ERRORS';
+
 
 export const receivePost = post => ({
   type: RECEIVE_POST,
   post
+})
+
+export const receivePosts = posts => ({
+  type: RECEIVE_POSTS,
+  posts
 })
 
 export const receivePostErrors = errors => ({
--- frontend/src/reducers/posts_reducer.js
+++ frontend/src/reducers/posts_reducer.js
@@ -1,10 +1,13 @@
-import { RECEIVE_POST } from '../actions/post_actions';
+import { RECEIVE_POST,
+         RECEIVE_POSTS } from '../actions/post_actions';
 
 export default (state = {}, action) => {
   Object.freeze(state);
   switch(action.type) {
     case RECEIVE_POST:
       return {...state, [action.post.id]: action.post}
+    case RECEIVE_POSTS:
+      return action.posts;
     default:
       return state;
   }
