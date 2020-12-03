af21ae862f1c41a36935650688a638b5b970e7e7
jared-greenberg <jared.p.greenberg@gmail.com>
2020-12-03 17:27:57 -0500

    Fixed the normalized state for posts and users

--- frontend/src/reducers/posts_reducer.js
+++ frontend/src/reducers/posts_reducer.js
@@ -1,14 +1,19 @@
 import { RECEIVE_POST,
-         RECEIVE_POSTS } from '../actions/post_actions';
+         RECEIVE_POSTS,
+         RECEIVE_NEW_POST } from '../actions/post_actions';
 
 export default (state = {}, action) => {
+  debugger
   Object.freeze(state);
   switch(action.type) {
+    case RECEIVE_NEW_POST:
+      return {...state, [action.post.data._id]: action.post.data}
+    case RECEIVE_POSTS:
+      return action.posts.posts;
     case RECEIVE_POST:
       return {...state, [action.post._id]: action.post}
-    case RECEIVE_POSTS:
-      return action.posts;
     default:
       return state;
   }
+
 }
