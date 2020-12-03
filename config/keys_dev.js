66bac3fdfcc5edcf25e42600a3c50e3d033ad181
Tim Bedford <tmbdfd@gmail.com>
2020-12-03 13:47:40 -0500

    Rework keys for Heroku (part 2)

--- config/keys_dev.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-  mongoURI: 'mongodb+srv://lavafish:M3GyJD8AsIZR9lAq@cluster0.2k6b5.mongodb.net/menutube?retryWrites=true&w=majority',
-  secretOrKey: "secret"       //Make sure this is your own unique string
-}
