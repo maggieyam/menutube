bea50fdcb988b6aeb56dccddea045a0a6febe5d4
Maggie Yan <4everyan@gmail.com>
2020-12-03 21:05:00 -0500

    style postIndex feed page

--- frontend/src/reducers/posts_reducer.js
+++ frontend/src/reducers/posts_reducer.js
@@ -3,7 +3,7 @@ import { RECEIVE_POST,
          RECEIVE_NEW_POST } from '../actions/post_actions';
 
 export default (state = {}, action) => {
-  debugger
+  
   Object.freeze(state);
   switch(action.type) {
     case RECEIVE_NEW_POST:
