9a3127db5eefd9561c4b010d336012a79d8344e4
jared-greenberg <jared.p.greenberg@gmail.com>
2020-12-16 17:18:17 -0500

    Adjust saving a post to update the current userinfo under the session state

--- frontend/src/reducers/session_reducer.js
+++ frontend/src/reducers/session_reducer.js
@@ -1,5 +1,6 @@
 import { RECEIVE_USER_LOGOUT,
          RECEIVE_CURRENT_USER  } from '../actions/session_actions';
+import { RECEIVE_USER } from '../actions/user_actions';
 
 const defaultState = { 
   isAuthenticated: false,
@@ -19,6 +20,16 @@ export default (state = defaultState, action) => {
           isAuthenticated: !!action.currentUser,
           userInfo: action.currentUser
         }
+    case RECEIVE_USER:
+      debugger
+      if (action.user.data._id === state.userInfo.id){
+        let newState = {...state};
+        newState.userInfo.saved = action.user.data.saved;
+        return newState;
+      }
+      else {
+        return state;
+      }
     default:
       return state;
   }
