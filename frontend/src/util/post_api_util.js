a9a9924cdb32dd6f5191f8508d92f87fcc37d1b5
Tony <chentony17@gmail.com>
2020-12-15 20:29:37 -0500

    add frontend route

--- frontend/src/util/post_api_util.js
+++ frontend/src/util/post_api_util.js
@@ -24,3 +24,6 @@ export const savePost = (postId, body) => {
   return axios.post(`api/posts/save/${postId}`, body)
 }
 
+export const deletePost = (postId, body) => {
+  return axios.post(`api/posts/delete/${postId}`, body);
+};
