f0b4bfd904144f36bf4e855a7826997756d302af
Tim Bedford <tmbdfd@gmail.com>
2020-12-16 15:13:00 -0500

    Add edit form, which is basically just the create form. Will be changed much more soon.

---
+++ frontend/src/components/posts/post_edit_form.jsx
@@ -0,0 +1,169 @@
+import React from 'react';
+import { withRouter } from 'react-router-dom';
+import "react-loader-spinner/dist/loader/css/react-spinner-loader.css";
+import 'semantic-ui-css/semantic.min.css'
+import './create_post_form.css'
+import Loader from 'react-loader-spinner';
+import { uploadFile } from 'react-s3';
+import { Dropdown } from 'semantic-ui-react';
+const aws = require('../../config/aws');
+
+const config = {
+  bucketName: aws.BUCKET_NAME,
+  region: 'us-east-1',
+  accessKeyId: aws.ID,
+  secretAccessKey: aws.SECRET,
+}
+
+class EditPostForm extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = { title: props.post.title }
+    this.changeTitle = this.changeTitle.bind(this);
+    this.errors = this.errors.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.loaderSpinner = this.loaderSpinner.bind(this);
+    this.fileLoader = React.createRef();
+    this.handleTag = this.handleTag.bind(this);
+  }
+
+  componentDidMount() {
+    this.props.fetchTags()
+  }
+
+
+  changeTitle(e) {
+    e.preventDefault();
+    this.setState({ title: e.currentTarget.value });
+  }
+
+  errors(field) {
+    if (this.props.errors[field]) {
+      return <p className="posting-error">{this.props.errors[field]}</p>
+    }
+    return
+  }
+
+  handleTag(e, data) {
+    const bools = {};
+    data.value.map(category => bools[category] = true);
+    this.setState({ [data.placeholder.toLowerCase()]: bools })
+  }
+
+
+  handleSubmit(e) {
+    e.preventDefault();
+
+    const video = this.fileLoader.current.files[0]
+    this.props.loadingOn();
+    uploadFile(video, config).then(
+      data => {
+
+        const post = {
+          title: this.state.title,
+          url: data.location,
+          nutrition: this.state.nutrition,
+          diet: this.state.diet,
+          ingredients: this.state.ingredients
+        }
+        this.props.createPost(post).then(() => {
+          this.props.loadingOff();
+          this.props.clearPostErrors();
+          this.props.history.push('/feed')
+        })
+      }
+
+    ).catch(err => {
+      this.props.loadingOff()
+      console.log(err)
+    })
+  }
+
+  loaderSpinner() {
+    if (this.props.loading) {
+      return (
+        <Loader type="Grid" color="#ee82ee" height={100} width={100} />
+      )
+    }
+    return
+  }
+
+  optionify(category) {
+    const options = Object.keys(category).map(tag => {
+      return { key: tag, text: tag[0].toUpperCase() + tag.slice(1), value: tag }
+    })
+    options.pop();
+    return options
+  }
+
+
+  render() {
+
+    if (!this.props.diet) return null;
+    return (
+      <form className="post-form" onSubmit={this.handleSubmit}>
+        {this.loaderSpinner()}
+
+        <div className="title-input-div">
+          <label htmlFor="post-title">Enter a title:</label>
+          <input
+            id="post-title"
+            type="text"
+            value={this.state.title}
+            onChange={this.changeTitle} />
+          {this.errors("title")}
+        </div>
+
+        <div className="vid-input-div">
+          <input
+            id="video-input"
+            type="file"
+            ref={this.fileLoader}
+            accept="video/*" />
+
+        </div>
+
+        <label> Diet/Restrictions:
+          <Dropdown
+            placeholder='Diet'
+            fluid
+            multiple
+            search
+            selection
+            options={this.optionify(this.props.diet)}
+            onChange={this.handleTag}
+          />
+        </label>
+
+        <label> Nutrition:
+        <Dropdown
+            placeholder='Nutrition'
+            fluid
+            multiple
+            search
+            selection
+            options={this.optionify(this.props.nutrition)}
+            onChange={this.handleTag}
+          />
+        </label>
+
+        <label>Ingredients:
+        <Dropdown
+            placeholder='Ingredients'
+            fluid
+            multiple
+            search
+            selection
+            options={this.optionify(this.props.ingredients)}
+            onChange={this.handleTag}
+          />
+        </label>
+        <input id="submit-post" type="submit" value="Submit Post" />
+      </form>
+    )
+  }
+
+}
+
+export default withRouter(EditPostForm);
---
+++ frontend/src/components/posts/post_edit_form_container.js
@@ -0,0 +1,25 @@
+import { connect } from 'react-redux';
+import { editPost, clearPostErrors } from '../../actions/post_actions';
+import { loadingOn, loadingOff } from '../../actions/loading_actions';
+import { fetchTags } from '../../actions/tag_actions';
+import EditPostForm from './post_edit_form';
+
+const mStP = (state, ownProps) => ({
+  currentUser: state.session.userInfo,
+  errors: state.errors.post,
+  loading: state.ui.loading,
+  diet: state.entities.tags[0],
+  ingredients: state.entities.tags[1],
+  nutrition: state.entities.tags[2],
+  post: state.entities.posts[ownProps.match.params.id]
+})
+
+const mDtP = dispatch => ({
+  editPost: post => dispatch(editPost(post)),
+  loadingOn: () => dispatch(loadingOn()),
+  loadingOff: () => dispatch(loadingOff()),
+  clearPostErrors: () => dispatch(clearPostErrors()),
+  fetchTags: () => dispatch(fetchTags())
+})
+
+export default connect(mStP, mDtP)(EditPostForm);
