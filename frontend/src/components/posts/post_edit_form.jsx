530c4de4f17ae8d3262feba44866c17cc933dab5
Tim Bedford <tmbdfd@gmail.com>
2020-12-17 18:54:12 -0500

    Add steps with timestamps and descriptions

--- frontend/src/components/posts/post_edit_form.jsx
+++ frontend/src/components/posts/post_edit_form.jsx
@@ -15,12 +15,29 @@ const config = {
   secretAccessKey: aws.SECRET,
 }
 
+const convertStepTimeToSeconds = step => {
+  const { minutes, seconds } = step;
+  step["timestamp"] = (parseInt(minutes) * 60) + parseInt(seconds); 
+  delete step["minutes"];
+  delete step["seconds"];
+  return step;
+}
+
+const convertStepTimeToMinutesAndSeconds = step => {
+  const { timestamp } = step;
+  step["minutes"] = Math.floor(timestamp / 60);
+  step["seconds"] = timestamp % 60;
+  delete step["timestamp"];
+  return step;
+}
+
 class EditPostForm extends React.Component {
 
   constructor(props) {
     super(props);
     this.state = {
-      title: ""
+      title: "",
+      steps: []
     }
     this.changeTitle = this.changeTitle.bind(this);
     this.errors = this.errors.bind(this);
@@ -28,18 +45,30 @@ class EditPostForm extends React.Component {
     this.loaderSpinner = this.loaderSpinner.bind(this);
     this.fileLoader = React.createRef();
     this.handleTag = this.handleTag.bind(this);
+    this.changeStep = this.changeStep.bind(this);
+    this.addStep = this.addStep.bind(this);
+    this.removeStep = this.removeStep.bind(this);
   }
 
   componentDidMount() {
     this.props.fetchTags();
     this.props.fetchPost(this.props.match.params.id).then(({post}) => {
-      this.setState({...post})
+      this.setState({
+        title: post.title,
+        steps: post.steps.map(step => convertStepTimeToMinutesAndSeconds(step))
+      })
     });
   }
 
   changeTitle(e) {
     e.preventDefault();
     this.setState({ title: e.currentTarget.value });
+  }
+
+  changeStep(e, type, idx) {
+    let { steps } = this.state;
+    steps[idx][type] = e.target.value;
+    this.setState({ steps });
   }
 
   errors(field) {
@@ -60,7 +89,8 @@ class EditPostForm extends React.Component {
 
     this.props.loadingOn();
     const editedPost = {
-      title: this.state.title
+      title: this.state.title,
+      steps: this.state.steps.map(step => convertStepTimeToSeconds(step))
     }
     this.props.editPost(this.props.post._id, editedPost).then(() => {
       this.props.loadingOff();
@@ -86,8 +116,54 @@ class EditPostForm extends React.Component {
     return options
   }
 
+  addStep() {
+    this.setState({
+      steps: this.state.steps.concat([{
+        description: "",
+        minutes: 0,
+        seconds: 0
+      }])
+    })
+  }
+
+  removeStep(e, idx) {
+    e.preventDefault();
+    let { steps } = this.state;
+    steps.splice(idx, 1);
+    this.setState({ steps });
+  }
+
   render() {
     if (!this.props.diet) return null;
+    
+    const { steps } = this.state;
+    const stepsList = (
+      <div className="steps-input-div">
+        {steps.map(({minutes, seconds, description}, idx) => (
+            <div key={idx} className="step">
+              <input
+                type="number" max="19" min="0" value={minutes}
+                onChange={e => this.changeStep(e, "minutes", idx)}/>
+              :
+              <input
+                type="number" max="59" min="0" value={seconds}
+                onChange={e => this.changeStep(e, "seconds", idx)}
+              />
+              <input
+                type="text" maxLength="100" value={description}
+                onChange={e => this.changeStep(e, "description", idx)}
+              />
+              <button type="button" onClick={e => this.removeStep(e, idx)}>
+                Remove
+              </button>
+            </div>
+          )
+        )}
+        <button type="button" onClick={this.addStep}>
+          {steps.length === 0 ? "Begin adding steps" : "Add another step"}
+        </button>
+      </div>
+    )
     
     return (
       <form className="post-form" onSubmit={this.handleSubmit}>
@@ -102,6 +178,10 @@ class EditPostForm extends React.Component {
             onChange={this.changeTitle} />
           {this.errors("title")}
         </div>
+
+        <label>Steps:
+          {stepsList}
+        </label>
         <input id="submit-post" type="submit" value="Apply Changes" />
       </form>
     )
