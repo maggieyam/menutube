30ee5ce4cd443578c8219ff157e2f6f1b5cbe03f
Tony <chentony17@gmail.com>
2020-12-30 16:49:36 -0500

    update splash css

--- frontend/src/components/splash/splash_page.jsx
+++ frontend/src/components/splash/splash_page.jsx
@@ -1,13 +1,18 @@
 import React from "react";
-
+import dessert from "../../images/cooking.gif";
 import "./splash_page.css";
 
-import ScrollPage from "./parallax";
-
+// import ScrollPage from "./parallax";
 
 const SplashPage = () => {
-
-  return <ScrollPage />;
+  return (
+    <div className="splash-pg">
+      <img src={dessert} style={{ width: "20%" }} />
+      <h1 className="splash-welcome" style={{ fontSize: "25px" }}>
+        Welcome to MenuTube
+      </h1>
+    </div>
+  );
 };
 
 export default SplashPage;
