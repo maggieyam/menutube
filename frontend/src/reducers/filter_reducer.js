3dafd96b5f8c350c626f49df1aa57d789891e54c
Tim Bedford <tmbdfd@gmail.com>
2020-12-29 20:21:59 -0500

    Update filter_reducer.js
    Remove console.log statement

--- frontend/src/reducers/filter_reducer.js
+++ frontend/src/reducers/filter_reducer.js
@@ -4,7 +4,6 @@ import { merge, mergeWith } from 'lodash';
 export default (state = {}, action) => {
   switch(action.type){
     case UPDATE_FILTER: 
-      console.log(merge({}, {a: [2, 1]}, {a: []}))
       let copy = merge({}, state);
       let newState = mergeWith(copy, action.filter, customizer)
       return newState;
