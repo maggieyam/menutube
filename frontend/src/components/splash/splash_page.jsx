8c890623ca13e89d91ce2f278ac02f9ea5f3b309
Tony <chentony17@gmail.com>
2020-12-31 17:09:06 -0500

    update css

--- frontend/src/components/splash/splash_page.jsx
+++ frontend/src/components/splash/splash_page.jsx
@@ -1,23 +1,49 @@
 import React from "react";
 import dessert from "../../images/cooking.gif";
 import "./splash_page.css";
-
+import { connect } from "react-redux";
+import { openModal } from "../../actions/modal_actions";
+import { logout } from "../../actions/session_actions";
 // import ScrollPage from "./parallax";
 
-const SplashPage = () => {
-  return (
-    <div className="splash-pg">
-      <img className="splash-img" src={dessert} style={{ width: "40%" }} />
-      <span className="splash-welcome" style={{ fontSize: "35px" }}>
-        Welcome to MenuTube
-      </span>
-      {/* <div>
-        <footer>
-          Create recipe tutorials and post them.
-        </footer>
-      </div> */}
-    </div>
-  );
-};
+const mDTP = (dispatch) => ({
+  logout: () => dispatch(logout()),
+  openModal: (modalType) => dispatch(openModal(modalType)),
+});
 
-export default SplashPage;
+class SplashPage extends React.Component {
+  render() {
+    return (
+      <div className="splash-pg">
+        <div className="splash-welcome">
+          <img className="splash-img" src={dessert} />
+          <div className="splash-intro">
+            <h1>MenuTube</h1>
+            <div className="splash-desc">
+              <span className="splash-text">
+                Create recipe tutorials and post them.
+              </span>
+              <span className="splash-text">
+                Create recipe tutorials and post them.
+              </span>
+            </div>
+            <div
+              className="signup-now"
+              style={{ backgroundColor: "transparent" }}
+            >
+              <button
+                onClick={() => this.props.openModal("signup")}
+                className="session-btn splash-session-btn"
+                id="signup"
+              >
+                Join Us
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default connect(null, mDTP)(SplashPage);
