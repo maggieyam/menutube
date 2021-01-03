a5652dec787ee18b084d8794984f78e06c46fefb
jared-greenberg <jared.p.greenberg@gmail.com>
2021-01-03 00:01:50 -0500

    Fix changes from github review

--- frontend/src/components/posts/post_show.jsx
+++ frontend/src/components/posts/post_show.jsx
@@ -106,6 +106,11 @@ class PostShow extends Component {
           <div className="video-header">
             <div className="left-side-header">
               <p className="show-title">{post.title}</p>
+              {/* <p>by <Link
+                className='profile-link'
+                to={`/profile/${post.user._id}`}
+              >{post.user.username}</Link>
+              </p> */}
               <p className="author">by {post.user.username}</p>
             </div>
             {postButtons}
